266cc1292dcc4b3a78df5c5ad0eedc2643e7242a
Sebastian Scheibe <sebastian@ecostack.nl>
2024-04-25 12:39:49 +0800

    add firmware update case CWE-494 for a08

--- src/a08-software-and-data-integrity-failures/a08-software-and-data-integrity-failures.test.ts
+++ src/a08-software-and-data-integrity-failures/a08-software-and-data-integrity-failures.test.ts
@@ -1,25 +1,27 @@
 import {describe, test} from 'node:test';
 import request from "supertest";
-import {getServer} from "./a08-software-and-data-integrity-failures";
+import {getServer, signData} from "./a08-software-and-data-integrity-failures";
 
 describe('a08-software-and-data-integrity-failures', () => {
-    test('check root endpoint', async () => {
-        await request(getServer()).get('/').expect(200).expect('hello world');
-    })
+    describe('CWE-494: Download of Code Without Integrity Check', () => {
+        test('update firmware signed', async () => {
+            const data = 'The new firmware update is available.'
+            const signature = signData(data)
 
-    test('check secret endpoint with auth', async () => {
-        await request(getServer()).get('/secret-endpoint').auth('admin', 'secret-password').expect(200).expect("The answer is 42");
+            await request(getServer()).post('/update-firmware').send({data, signature}).expect(200)
+        })
+        test('update firmware unsigned', async () => {
+            const data = 'Malicious firmware update'
+            await request(getServer()).post('/update-firmware').send({data}).expect(401)
+        })
     })
+    describe('CWE-829: Inclusion of Functionality from Untrusted Control Sphere', () => {
+        test('load module allowed', async () => {
+            await request(getServer()).get('/load-module/' + encodeURIComponent('allowed.js')).expect(200)
+        })
 
-    test('check secret endpoint without auth', async () => {
-        await request(getServer()).get('/secret-endpoint').expect(401)
-    })
-
-    test('load module allowed', async () => {
-        await request(getServer()).get('/load-module/'+encodeURIComponent('allowed.js')).expect(200)
-    })
-
-    test('load module disallowed', async () => {
-        await request(getServer()).get('/load-module/'+encodeURIComponent('disallowed.js')).expect(500)
+        test('load module disallowed', async () => {
+            await request(getServer()).get('/load-module/' + encodeURIComponent('disallowed.js')).expect(500)
+        })
     })
 })
