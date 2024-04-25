02e5cf283fd3df15f181bd45f082930459e956cf
Sebastian Scheibe <sebastian@ecostack.nl>
2024-04-25 12:49:09 +0800

    add second case for security logging

--- src/a09-security-logging-and-monitoring-failures/a09-security-logging-and-monitoring-failures.test.ts
+++ src/a09-security-logging-and-monitoring-failures/a09-security-logging-and-monitoring-failures.test.ts
@@ -12,15 +12,48 @@ class CustomLoggerImpl implements CustomLogger {
 }
 
 describe('a09-security-logging-and-monitoring-failures', () => {
-    test('safe logging practise', async () => {
-        const logger = new CustomLoggerImpl()
-        await request(getServer(logger)).post('/login').send({username:'admin',password:'secret'}).expect(200).expect("Logged in");
+    describe('CWE-778: Insufficient Logging', () => {
+        test('login event should be logged', async () => {
+            const logger = new CustomLoggerImpl()
+            await request(getServer(logger)).post('/login').send({username:'admin',password:'secret'}).expect(200).expect("Logged in");
 
-        assert.ok(logger.logMessages.length > 0)
-        for (const loggerElement of logger.logMessages) {
-            if (loggerElement.includes('password')) {
-                assert.fail('Password found in logs')
+            assert.ok(logger.logMessages.length > 0, 'No log messages found')
+            let foundLogin = false
+            for (const loggerElement of logger.logMessages) {
+                if (loggerElement.includes('Event: User Login')) {
+                    foundLogin = true
+                }
             }
-        }
+            if (!foundLogin) {
+                assert.fail('No login event found in logs')
+            }
+        })
     })
+
+    describe('CWE-532: Insertion of Sensitive Information into Log File', () => {
+        test('login should not contain password', async () => {
+            const logger = new CustomLoggerImpl()
+            await request(getServer(logger)).post('/login').send({username:'admin',password:'secret'}).expect(200).expect("Logged in");
+
+            assert.ok(logger.logMessages.length > 0, 'No log messages found')
+            for (const loggerElement of logger.logMessages) {
+                if (loggerElement.includes('password')) {
+                    assert.fail('Password found in logs')
+                }
+            }
+        })
+
+        test('payment log should not contain credit card number', async () => {
+            const logger = new CustomLoggerImpl()
+            await request(getServer(logger)).post('/pay').send({creditCard: "1234-5678-9012-3456"}).expect(200).expect("Payment received");
+
+            assert.ok(logger.logMessages.length > 0, 'No log messages found')
+            for (const loggerElement of logger.logMessages) {
+                if (loggerElement.includes('1234-5678-9012-3456')) {
+                    assert.fail('Credit card number found in logs')
+                }
+            }
+        })
+    })
+
 })
--- src/a09-security-logging-and-monitoring-failures/a09-security-logging-and-monitoring-failures.ts
+++ src/a09-security-logging-and-monitoring-failures/a09-security-logging-and-monitoring-failures.ts
@@ -11,10 +11,23 @@ export function getServer(logger:CustomLogger) {
 
     app.use(bodyParser.json())
 
+    app.post('/pay', (req:express.Request, res) => {
+        // TODO practise safe logging, remove sensitive data before logging
+        // see https://cwe.mitre.org/data/definitions/532.html
+        // CWE-117: Improper Output Neutralization for Logs
+        // see CWE-117: Improper Output Neutralization for Logs
+        logger.log(JSON.stringify(req.body))
+        res.send('Payment received')
+    })
+
     app.post('/login', (req:express.Request, res) => {
         // TODO practise safe logging, remove sensitive data before logging
         // see https://cwe.mitre.org/data/definitions/532.html
         logger.log(JSON.stringify(req.body))
+
+        // TODO log the event
+        // see https://cwe.mitre.org/data/definitions/778.html
+        // log the event of a user logging in, check the test case for exact format
         res.send('Logged in')
     })
 
